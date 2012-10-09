e486834be5012fd8684c413887f2d258e62e5a58
toran billups <toranb@gmail.com>
2012-10-08 19:53:38 -0500

    added sessions view/controller/route

--- codecamp/static/script/app/codecamp.js
+++ codecamp/static/script/app/codecamp.js
@@ -6,10 +6,25 @@ CodeCamp.ApplicationView = Ember.View.extend({
   templateName: 'application'
 });
 
+CodeCamp.Session = Ember.Object.extend({
+  name: ''
+});
+
+CodeCamp.SessionsView = Ember.View.extend({
+  templateName: 'sessions'
+});
+
+CodeCamp.SessionsController = Ember.ArrayController.extend({
+  content: []
+});
+
 CodeCamp.Router = Ember.Router.extend({
   root: Ember.Route.extend({
     index: Ember.Route.extend({
-      route: '/'
+      route: '/',
+      connectOutlets: function(router, context) {
+        router.get('applicationController').connectOutlet('sessions');
+      }
     })
   })
 });
