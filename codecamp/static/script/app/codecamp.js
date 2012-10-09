6eca93383531f8712b59eea05a666da02558a349
toran billups <toranb@gmail.com>
2012-10-08 19:43:50 -0500

    The Smallest Viable Ember Application

--- codecamp/static/script/app/codecamp.js
+++ codecamp/static/script/app/codecamp.js
@@ -1,1 +1,17 @@
+CodeCamp = Ember.Application.create();
 
+CodeCamp.ApplicationController = Ember.ObjectController.extend();
+
+CodeCamp.ApplicationView = Ember.View.extend({
+  templateName: 'application'
+});
+
+CodeCamp.Router = Ember.Router.extend({
+  root: Ember.Route.extend({
+    index: Ember.Route.extend({
+      route: '/'
+    })
+  })
+});
+
+CodeCamp.initialize();
